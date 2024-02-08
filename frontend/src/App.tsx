1360b0be00a6601f449a1b9396d1a195c1a8c53d
SoulYaKnow <ranel.soliano@gmail.com>
2024-02-08 11:40:36 +1100

    updated admin profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import HomePage from "./components/LandingPage";
+import ProfilePage from "./components/hr/AdminProfile";
 function App() {
-  return <HomePage />;
+  return <ProfilePage />;
 }
 
 export default App;
