f099d192a9dd70f05cfbff0c1935c89a9171ae2d
skysa <carcelaarnel@gmail.comm>
2024-02-12 13:13:00 +0800

    dashboard

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -4,22 +4,25 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
 import "../App.css" 
 function Calendar(){
    
     return (
         <>
             <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+                
+                initialView={"dayGridMonth"}
                 headerToolbar={
                     {
-                        start: 'title', // will normally be on the left. if RTL, will be on the right
-                        center: '',
-                        end: 'prev,next' 
+                        start: 'prev,next', // will normally be on the left. if RTL, will be on the right
+                        center: 'title',
+                        end: 'dayGridMonth' 
                     }
                 }
-                height={'60vh'}
-                
+                height={'400px'}
+                // ,timeGridDay,timeGridWeek,listDay
             />
             
         </>
