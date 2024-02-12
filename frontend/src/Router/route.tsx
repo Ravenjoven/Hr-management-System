e22092eeffdab898ae194bc26128c861fd830689
skysa <carcelaarnel@gmail.comm>
2024-02-13 07:52:52 +0800

    minor changes

--- frontend/src/Router/route.tsx
+++ frontend/src/Router/route.tsx
@@ -16,7 +16,7 @@ function router() {
           <Route index path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Employee" element={<AdminUserList />} />
+          <Route path="/EmployeeList" element={<AdminUserList />} />
           <Route path="/Category" element={<AdminJobCategory />} />
           <Route path="/Attendance" element={<AdminAttendance />} />
           <Route path="/Profile" element={<Profile />} />
