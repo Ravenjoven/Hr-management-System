0373eb74341ae5b9de5b553b6678fe088a2d84c6
skysa <carcelaarnel@gmail.comm>
2024-02-14 09:30:36 +0800

    calendar changes

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -6,27 +6,30 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import listPlugin from '@fullcalendar/list'
 import "../App.css" 
-function Calendar(){
-   
-    return (
-        <>
-            <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
-                
-                initialView={"dayGridMonth"}
-                headerToolbar={
-                    {
-                        start: 'prev,next', // will normally be on the left. if RTL, will be on the right
-                        center: 'title',
-                        end: 'dayGridMonth' 
-                    }
-                }
-                height={'400px'}
-                // ,timeGridDay,timeGridWeek,listDay
-            />
-            
-        </>
-    );
-}
-export default Calendar;
+
+export default class Calendar extends React.Component {
+    render() {
+      return (
+        <FullCalendar
+          plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin ]}
+          initialView="dayGridMonth"
+          dateClick={this.handleDateClick}
+          headerToolbar={
+            {
+                start: 'prev,next', // will normally be on the left. if RTL, will be on the right
+                center: 'title',
+                end: 'dayGridMonth' 
+            }
+        }
+        />
+      )
+    }
+    handleDateClick = (arg) => { // bind with an arrow function
+        alert(arg.dateStr)
+      }
+    
+  }
+
+
+
 
