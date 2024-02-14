0e0657c095cb485cf0621d591087ec17afb1a313
skyssa <carcelaarnel@gmail.com>
2024-02-15 07:52:46 +0800

    changes in Login

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navar from "./Navar";
 const data = [
   {
     id: 1,
@@ -13,70 +14,7 @@ const Login = () => {
   return (
     <div className="flex flex-col h-screen justify-between min-h-screen max-w-screen bg-white font-montserrat">
       <>
-        <nav>
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 h-full ml-8 mr-8">
-            <div className="flex items-center justify-start">
-              <img src="../images/img2.png" className="h-10"></img>
-              <img src="../images/img3.png" className="h-10 pt-2" />
-            </div>
-            <button
-              data-collapse-toggle="navbar-default"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ml-0"
-              aria-controls="navbar-default"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-            <div
-              className="hidden w-full md:block md:w-auto mr-4"
-              id="navbar-default"
-            >
-              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-20 rtl:space-x-reverse md:mt-0 md:border-0 transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-orange-400 md:p-0 dark:text-white md:dark:text-blue-500"
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 md:text-custom-text-gray rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    About us
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 md:text-custom-text-gray rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navar />
         <section className="max-w-screen-xl flex ml-8 mr-8">
           <div className="w-full pl-5 pt-2">
             <svg
@@ -142,9 +80,9 @@ const Login = () => {
                 </Link>
               </button>
               <div className="mt-4 grid grid-cols-3 items-center text-black">
-                  <hr className="border-black" />
-                  <p className="text-center text-sm">OR</p>
-                  <hr className="border-black" />
+                <hr className="border-black" />
+                <p className="text-center text-sm">OR</p>
+                <hr className="border-black" />
               </div>
 
               <button
@@ -174,14 +112,14 @@ const Login = () => {
             </form>
           </div>
         </section>
-        <footer className="w-full bg-gray-600 static bottom-0 text-center text-white p-4 text-xs">
+        <footer className="w-full bg-custom-bg-black static bottom-0 text-center text-white p-4 text-xs">
           <div className="grid content-end">
             <div className="flex justify-center ">
-              <img src="../images/img2.png" className="w-[60px] h-10 "></img>
+              <img src="./images/footer-logo.png" className="h-10 w-[60px]" />
             </div>
             <br />
-            Copyright &copy; 2024 TERAVAULT, INC. dba TeraVault Software. All
-            rights reserved
+            Copyright @ 2024 TERAVAULT, INC. dba Teravault Software. All
+              rights reversed
           </div>
         </footer>
       </>
