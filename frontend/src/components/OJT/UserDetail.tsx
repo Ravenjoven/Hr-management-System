42d823ab376afd550a63157552d20ad27de9a388
skyssa <carcelaarnel@gmail.com>
2024-02-15 11:38:27 +0800

    user datail

--- frontend/src/components/OJT/UserDetail.tsx
+++ frontend/src/components/OJT/UserDetail.tsx
@@ -48,10 +48,12 @@ function UserProfile() {
             }`}
           >
             <div className="upper-div md:min-w-full h-full font-bol rounded flex bg-white text-black p-4">
-              <div><FontAwesomeIcon
-                icon={faUser}
-                className="flex w-[105px] h-[100px] border-[5px] border-gray-300 rounded-full p-3 m-8"
-              /></div>
+              <div>
+                <FontAwesomeIcon
+                  icon={faUser}
+                  className="flex w-[105px] h-[100px] border-[5px] border-gray-300 rounded-full p-3 m-8"
+                />
+              </div>
               <div className=" flex flex-col text-center justify-center items-center">
                 <h4 className="text-custom-text-black font-bold">JOHN DOE</h4>
                 <span className="text-custom-text-gray font-semibold p-1">
@@ -60,133 +62,99 @@ function UserProfile() {
               </div>
               <div className="flex flex-col m-12">
                 <div>
-                <h4 className="text-custom-text-black font-bold">EMAIL</h4>
-                <span className="text-custom-text-gray font-semibold">johndoe@gmail.com</span>
+                  <h4 className="text-custom-text-black font-bold">EMAIL</h4>
+                  <span className="text-custom-text-gray font-semibold">
+                    johndoe@gmail.com
+                  </span>
                 </div>
                 <div>
-                <h4 className="text-custom-text-black font-bold">NUMBER</h4>
-                <span className="text-custom-text-gray font-semibold">1234567890</span>
+                  <h4 className="text-custom-text-black font-bold">NUMBER</h4>
+                  <span className="text-custom-text-gray font-semibold">
+                    1234567890
+                  </span>
                 </div>
               </div>
               <div className="flex flex-col m-12">
                 <div>
-                <h4 className="text-custom-text-black font-bold">MENTOR</h4>
-                <span className="text-custom-text-gray font-semibold">johndoe</span>
+                  <h4 className="text-custom-text-black font-bold">MENTOR</h4>
+                  <span className="text-custom-text-gray font-semibold">
+                    johndoe
+                  </span>
                 </div>
                 <div>
-                <h4 className="text-custom-text-black font-bold">SCHEDULE</h4>
-                <span className="text-custom-text-gray font-semibold">6am - 3pm</span>
+                  <h4 className="text-custom-text-black font-bold">SCHEDULE</h4>
+                  <span className="text-custom-text-gray font-semibold">
+                    6am - 3pm
+                  </span>
                 </div>
               </div>
             </div>
             <div className="flex">
-              <div className="left-div mt-4 w-[400px] min-h-screen border-[3px] rounded-2xl border-custom-text-orange">
-              <div className="font-bold text-custom-text-black my-4 ml-8 space-y-3">
-                    <div>
-                      Fullname :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        Teravault Inc
-                      </span>
-                    </div>
-                    <div>
-                      Date of Birth :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        00/00/0000
-                      </span>
-                    </div>
-                    <div>
-                      Place of Birth :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        Cebu Business Park
-                      </span>
-                    </div>
-                    <div>
-                      Address :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        Ayala Center, Cebu Business Park
-                      </span>
-                    </div>
-                    <div>
-                      Age :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        00
-                      </span>
-                    </div>
-                    <div>
-                      Gender :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        ---
-                      </span>
-                    </div>
-                    <div>
-                      Nationality :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        -----
-                      </span>
-                    </div>
-                    <div>
-                      Religion :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        ----
-                      </span>
-                    </div>
+              <div className="left-div mt-4 w-[400px] min-h-screen border-[3px] rounded-2xl bg-white text-black p-4">
+                <div className="font-bold text-custom-text-black my-4 ml-8 space-y-3">
+                <div>
+                    <h1 className="font-bold p-2" >PERSONAL INFO</h1>
+                </div>
+                  <div>
+                    Fullname :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      Teravault Inc
+                    </span>
                   </div>
+                  <div>
+                    Date of Birth :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      00/00/0000
+                    </span>
+                  </div>
+                  <div>
+                    Place of Birth :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      Cebu Business Park
+                    </span>
+                  </div>
+                  <div>
+                    Address :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      Ayala Center, Cebu Business Park
+                    </span>
+                  </div>
+                  <div>
+                    Age :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      00
+                    </span>
+                  </div>
+                  <div>
+                    Gender :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      ---
+                    </span>
+                  </div>
+                  <div>
+                    Nationality :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      -----
+                    </span>
+                  </div>
+                  <div>
+                    Religion :{" "}
+                    <span className="text-custom-text-gray font-semibold p-2">
+                      ----
+                    </span>
+                  </div>
+                </div>
               </div>
               <div className="right-div w-full h-full mt-4 md:overflow-hidden overflow-x-scroll">
-                <div className="border-[3px] border-custom-text-orange md:ml-4 rounded-2xl">
+                <div className="border-[3px] bg-white text-black md:ml-4 rounded-2xl">
                   <div className="font-bold text-custom-text-black my-4 ml-8 space-y-3">
                     <div>
-                      Fullname :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        Teravault Inc
-                      </span>
-                    </div>
-                    <div>
-                      Date of Birth :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        00/00/0000
-                      </span>
-                    </div>
-                    <div>
-                      Place of Birth :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        Cebu Business Park
-                      </span>
-                    </div>
-                    <div>
-                      Address :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        Ayala Center, Cebu Business Park
-                      </span>
-                    </div>
-                    <div>
-                      Age :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        00
-                      </span>
-                    </div>
-                    <div>
-                      Gender :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        ---
-                      </span>
-                    </div>
-                    <div>
-                      Nationality :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        -----
-                      </span>
-                    </div>
-                    <div>
-                      Religion :{" "}
-                      <span className="text-custom-text-gray font-semibold p-2">
-                        ----
-                      </span>
+                        <h1>SKILLS</h1>
                     </div>
                   </div>
                 </div>
                 <div className="w-full h-full">
-                  <div className="border-[3px] border-custom-text-orange md:ml-4 rounded-2xl mt-4">
+                  <div className="border-[3px] bg-white text-black md:ml-4 rounded-2xl mt-4">
                     <h3 className="my-4 uppercase font-bold md:ml-8 text-xl text-custom-text-black">
                       certification
                     </h3>
