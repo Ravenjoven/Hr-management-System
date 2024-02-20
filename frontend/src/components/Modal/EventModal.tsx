07c384f6ac2f0e88d5a0d48d069763a4888967da
skyssa <carcelaarnel@gmail.com>
2024-02-20 14:11:25 +0800

    notification update

--- frontend/src/components/Modal/EventModal.tsx
+++ frontend/src/components/Modal/EventModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 interface ModalProps {
   onClose: () => void;
@@ -10,7 +10,10 @@ const ModalComponent: React.FC<ModalProps> = ({ start, date, onClose }) => {
   const handleClose = () => {
     onClose && onClose();
   };
-
+const [isAdd, setIsAdd] = useState(false);
+const handleOpenAdd=()=>{
+  setIsAdd(true);
+}
   return (
     <>
       <div className="fixed z-50 inset-0 overflow-y-auto">
@@ -38,10 +41,11 @@ const ModalComponent: React.FC<ModalProps> = ({ start, date, onClose }) => {
               <div className="mt-3 text-center sm:mt-0 sm:text-left">
                 <span>Events And Reminders</span>
                 <hr />
-                <div  className="w-full flex flex-col mb-2 justify-center items-center hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
-              >
-                  <div className="w-full p-2 flex justify-center items-center">+</div>
-                  <p className="flex justify-center items-center">There's no event or reminder</p>
+                <div className="w-full p-8 flex flex-col mb-2 justify-center items-center  sm:ml-3 sm:w-auto sm:text-sm">
+                 
+                  <p className="flex justify-center items-center">
+                    There's no event or reminder
+                  </p>
                 </div>
                 {/* <div className="mt-4 text-black flex flex-col">
                   <div className="flex flex-col p-4">
@@ -76,6 +80,84 @@ const ModalComponent: React.FC<ModalProps> = ({ start, date, onClose }) => {
             </div>
             <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse ">
               <button
+              onClick={handleOpenAdd} 
+                type="button"
+                className="w-full md:inline-flex inline-block mb-2 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Add 
+              </button>
+              <button
+                type="button"
+                onClick={handleClose}
+                className="w-full md:inline-flex inlune-block mb-2 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {isAdd &&(
+        <div className="fixed z-50 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-w-screen min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+          <div
+            className="fixed inset-0 transition-opacity"
+            aria-hidden="true"
+            onClick={handleClose}
+          >
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <div
+            className="inline-block bg-white w-full rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-headline"
+          >
+            <div className="text-custom-text-black">
+              <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                <span>Events And Reminders</span>
+                <hr />
+               
+                <div className="mt-4 text-black flex flex-col">
+                  <div className="flex flex-col p-4">
+                    <div>
+                      <label htmlFor="firstname">Event</label>
+                      <input
+                        type="text"
+                        placeholder="Create Event"
+                        className="border border-custom-text-gray rounded pl-2 w-full h-10"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="date-of-Event">Date</label>
+                      <input
+                        type="date"
+                        placeholder="Event"
+                        value={date}
+                        className="border border-custom-text-gray rounded pl-2 w-full h-10"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="time-of-Event">HH:MM AM/PM</label>
+                      <input
+                        type="time"
+                        placeholder="Time"
+                        className="border border-custom-text-gray rounded pl-2 w-full h-10"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse ">
+              <button
                 type="button"
                 className="w-full md:inline-flex inline-block mb-2 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
               >
@@ -92,6 +174,7 @@ const ModalComponent: React.FC<ModalProps> = ({ start, date, onClose }) => {
           </div>
         </div>
       </div>
+      )}
     </>
   );
 };
