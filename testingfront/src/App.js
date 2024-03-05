4f9f9078676731367fb3a4601fc81481245231e4
Raven <ravenjoven15@gmal.com>
2024-03-05 08:53:27 +0800

    Jobposting backend almost done

--- testingfront/src/App.js
+++ testingfront/src/App.js
@@ -4,18 +4,47 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import { theme } from './theme';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LogIn from './pages/LogIn';
+import UserDashboard from './pages/user/UserDashboard';
+import UserRoute from './component/UserRoute';
+import AdminRoute from './component/AdminRoute';
+import Layout from './pages/global/Layout';
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import UserJobsHistory from './pages/user/UserJobsHistory';
+import UserInfoDashboard from './pages/user/UserInfoDashboard';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import SingleJob from './pages/SingleJob';
+//HOC
+const UserDashboardHOC = Layout(UserDashboard)
+const UserJobsHistoryHOC = Layout(UserJobsHistory)
+const UserInfoDashboardHOC = Layout(UserInfoDashboard)
+const AdminDashboardHOC = Layout(AdminDashboard)
+
 
 const App = () => {
     return (
         <>
+            <ToastContainer/>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
+                <ProSidebarProvider>
                 <BrowserRouter>
                     <Routes>
                         <Route path='/' element={<Home />} />
+                        <Route path='/search/location/:location' element={<Home/>}/>
+                        <Route path='/search/:keyword' element={<Home/>}/>
+                        <Route path='/login' element={<LogIn/>}/>
+                        <Route path='/job/:id' element={<SingleJob/>}/>
+                        <Route path='/admin/dashboard' element={<AdminRoute><AdminDashboardHOC /></AdminRoute>} />
+                        <Route path='/user/dashboard' element={<UserRoute><UserDashboardHOC/></UserRoute>}/>
+                        <Route path='/user/jobs' element={<UserRoute><UserJobsHistoryHOC/></UserRoute>}/>
+                        <Route path='/user/info' element={<UserRoute><UserInfoDashboardHOC/></UserRoute>}/>
                         <Route path='*' element={<NotFound />} />
                     </Routes>
                 </BrowserRouter>
+                </ProSidebarProvider>
             </ThemeProvider>
         </>
     );
