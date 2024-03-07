bc008e5de8b75f9ab7250b252088506fb7c59df6
jezrael18 <jezraelsuliano507@gmail.com>
2024-03-08 07:46:08 +0800

    remove ojtlogin

--- frontend/src/Router/route.tsx
+++ frontend/src/Router/route.tsx
@@ -13,7 +13,6 @@ import Jobs from "../components/hr/Jobs";
 import OjtJobList from "../components/OJT/OjtJobs";
 import Applications from "../components/OJT/Applications";
 import CompDetails from "../components/hr/CompanyDetails";
-import OjtLogin from "../components/OJT/OjtLogin";
 import UserDetail from "../components/OJT/UserDetail";
 import OjtAttendance from "../components/OJT/OjtAttendance";
 import FileLeaves from "../components/OJT/FileLeave";
