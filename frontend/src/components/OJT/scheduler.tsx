10ef2592249372b107bb5d9de58e8e4e26695f9b
skyssa <carcelaarnel@gmail.com>
2024-03-19 14:28:34 +0800

    update 03/19

--- frontend/src/components/OJT/scheduler.tsx
+++ frontend/src/components/OJT/scheduler.tsx
@@ -3,12 +3,13 @@ import { Scheduler } from "../Calendar";
 function sched() {
   return (
     <div>
-      <div className="flex grid-col align-center justify-center">
+      <div className="flex grid-col items-center justify-center">
         <div className="border border-black w-full h-full m-4 flex grid-row">
-          <div className="flex items-center justify-center">
-            <img src="../images/img2.png" className="h-10"></img>
-            <img src="../images/img3.png" className="h-10 pt-2" />
+          <div className="flex mx-auto p-12">
+            <img src="../images/img2.png" className="h-10 "></img>
+            <img src="../images/img3.png" className="h-10 pt-2 " />
           </div>
+          
         </div>
         <div className="border border-black w-full h-full m-4">
           <Scheduler />
