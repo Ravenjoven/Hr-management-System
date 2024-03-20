c7f644ef8b42725e1c2558edaa348088e1ff45aa
jezrael18 <jezraelsuliano507@gmail.com>
2024-03-20 08:11:05 +0800

    progress 3/20

--- frontend/src/components/OJT/scheduler.tsx
+++ frontend/src/components/OJT/scheduler.tsx
@@ -4,10 +4,13 @@ function sched() {
   return (
     <div>
       <div className="flex grid-col items-center justify-center">
-        <div className="border border-black w-full h-full m-4 flex grid-row">
-          <div className="flex mx-auto p-12">
+        <div className="grid grid-rows-4 m-4">
+          <div className="border border-black flex w-full h-full mx-16  p-12">
             <img src="../images/img2.png" className="h-10 "></img>
             <img src="../images/img3.png" className="h-10 pt-2 " />
+          </div>
+          <div className="border border-black flex mx-auto p-12">
+            <h1>PEARL</h1>
           </div>
           
         </div>
