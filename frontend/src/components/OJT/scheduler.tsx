fb95328588a72d413600c004dbce336f03b7d116
jezrael18 <jezraelsuliano507@gmail.com>
2024-03-26 08:55:48 +0800

    scheduler

--- frontend/src/components/OJT/scheduler.tsx
+++ frontend/src/components/OJT/scheduler.tsx
@@ -1,50 +1,22 @@
 import React from "react";
 import { Scheduler } from "../Calendar";
+import OjtNavar from "./OjtNavar";
 function sched() {
   return (
-    <section>
-      <div className="w-screen h-screen flex grid-col-2">
-        <div className="w-1/2 h-full ">
-          <div className="w-full h-1/4">
-            <div className="flex justify-center items-center pt-12">
-              <img src="../images/img2.png" className="w-20 h-20"></img>
-              <img src="../images/img3.png" className="w-1/2 h-20" />
-            </div>
-          </div>
-          <div className="w-full h-3/4 bg-white border border-black">
-          
-            <div className="px-8">
-              <div>
-                <p className="text-5xl">Pearl</p>
-              </div>
-              <div>
-                <p className="text-2xl">Recuitment: Initial Interview</p>
-              </div>
-              <div>
-                <p className="text-2xl">30 Min</p>
-              </div>
-              <div>
-                <p className="text-2xl">Web Conferencing</p>
-              </div>
-            </div>
-            <div className="border border-black">
-              <div className="mx-auto w-3/4 ">
-                <p className="text-sm m-16" >you dont have a Events or reminder right now</p>
-              </div>
-              <div className="flex justify-center border border-black mt-12 mx-16">
-            <button className="bg-blue-800 p-2">Save</button>
-          </div>
-            </div>
-          </div>
-          
-        </div>
-        <div className="w-full h-full bg-gray-400">
-          <div className="w-4/5 m-auto bg-white  rounded-3xl">
-            <Scheduler />
+    <div className="max-h-screen max-w-screen  font-montserrat font-bold">
+      <OjtNavar />
+      <div className="flex m-4 h-full">
+        <div className="w-1/4 h-[90vh] rounded-tl-[40px] rounded-tr-none rounded-bl-[40px] bg-custom-bg-smooth  border border-black ">
+          <div className="absolute  object-cover border-2 border-black m-6">
+            <img src="../images/img2.png" className="w-1/4 h-1/2"></img>
+            <img src="../images/img3.png" className="w-1/2 h-1/2" />
           </div>
         </div>
+        <div className="w-3/4 h-[90vh] rounded-br-[40px] rounded-tr-[40px] bg-custom-text-orange border border-black"></div>
       </div>
-    </section>
+
+     
+    </div>
   );
 }
 export default sched;
